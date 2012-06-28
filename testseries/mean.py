ae722eaccb01b140b5f5bcb77b6fe5b0194821f2
Bartholomäus Dedersen <git@selbstapotheose.de>
2012-06-28 15:43:20 +0200

    mean calculator with csv export

--- testseries/mean.py
+++ testseries/mean.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 
 import sys
+import csv
+
+if len(sys.argv) < 2:
+    print "usage: " + sys.argv[0] + " [output file]"
+    sys.exit()
 
 j = 0
 i = 0
 with open(sys.argv[1]) as f:
-	for line in f:
-		i = i + int(line)
-		j += 1
+    binWriter = csv.writer(open(sys.argv[1] + ".csv", 'ab'))
+    for line in f:
+        binWriter.writerow(line)
+        i = i + int(line)
+        j += 1
+
 print i/j
-		
